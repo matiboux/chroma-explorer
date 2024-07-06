a112684ccb7f4d775b5d76dae33c5a5bbfaa628d
Matiboux <matiboux@gmail.com>
2024-07-06 22:06:22 +0200

    Move site config

---
+++ app/app/src/config.ts
@@ -0,0 +1,8 @@
+import type Site from '~/types/Site.d.ts'
+
+export const site: Site = {
+	title: 'Chroma Explorer',
+	description: 'Online tool to explore the contents of a Chroma database.',
+	author: 'Matiboux',
+	themeColor: '#ffffff',
+}
