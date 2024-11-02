7bae4e8ba4b1aed59384e76c35e20f3d779eb4fc
Matiboux <matiboux@gmail.com>
2024-11-02 17:07:45 +0100

    Update login form content & style

--- app/app/src/components/Login.svelte
+++ app/app/src/components/Login.svelte
@@ -11,8 +11,15 @@ const idSuffix = Math.random().toString(36).substring(2)
 		</label>
 		<input
 			type="text" name="chromadbServerUrl" required
+			placeholder="http://localhost:8000"
 			id={`chromadbServerUrl-${idSuffix}`}
 		/>
+		<p class="hint">
+			<span class="icon icon-[mdi--information-outline] icon-align"></span>
+			Root URL of the ChromaDB server.
+			The server must allow CORS from this domain or you can use a proxy:
+			try <a href="http://cors-proxy.matiboux.com/">CORS Proxy</a>.
+		</p>
 	</div>
 
 	<div class="input-group">
@@ -25,22 +32,20 @@ const idSuffix = Math.random().toString(36).substring(2)
 		/>
 	</div>
 
-	<button type="submit">Connect</button>
+	<div class="button-group">
+		<button type="submit">Connect</button>
+		<button type="reset">Reset</button>
+	</div>
 
 </form>
 
 <style lang="scss">
 form {
-	@apply flex flex-col;
-	@apply mx-2 mt-6 p-4 border-2 border-blue-200 rounded-md;
+	@apply flex flex-col gap-4;
+	@apply mt-4;
 
 	.input-group {
-		@apply flex flex-col;
-
-		+ .input-group {
-			@apply flex flex-col;
-			@apply mt-4;
-		}
+		@apply flex flex-col
 	}
 
 	label {
@@ -50,6 +55,45 @@ form {
 	input {
 		@apply px-2 py-1 border border-gray-300 rounded-md;
 		@apply border border-gray-300 rounded-md;
+
+		+ .hint {
+			@apply mt-1;
+		}
+	}
+
+	p.hint {
+		@apply text-sm text-gray-500;
+
+		a {
+			@apply
+			text-blue-500
+			hover:text-blue-600 hover:underline
+			;
+		}
+	}
+
+	.button-group {
+		@apply flex gap-4;
+	}
+
+	button {
+		@apply
+			bg-gray-400
+			enabled:hover:bg-gray-500
+			enabled:active:bg-gray-600
+			disabled:bg-gray-300
+			px-4 py-1 text-white font-semibold rounded-md
+			disabled:cursor-not-allowed
+			;
+
+		&[type="submit"] {
+			@apply
+				bg-blue-500
+				enabled:hover:bg-blue-600
+				enabled:active:bg-blue-700
+				disabled:bg-blue-300
+				;
+		}
 	}
 }
 </style>
