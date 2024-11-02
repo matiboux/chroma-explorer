67528b237f55aa508c1b40ad63f5c4f3d50ee804
Matiboux <matiboux@gmail.com>
2024-11-02 16:15:37 +0100

    Add login component

---
+++ app/app/src/components/Login.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+</script>
+
+<form>
+
+	<div class="input-group">
+		<label>ChromaDB Server URL:</label>
+		<input type="text" name="chromadbServerUrl" required>
+	</div>
+
+	<div class="input-group">
+		<label>Header Authorization Token:</label>
+		<input type="text" name="headerAuthorizationToken" required>
+	</div>
+
+	<button type="submit">Connect</button>
+
+</form>
+
+<style lang="scss">
+form {
+	@apply flex flex-col;
+	@apply mx-2 mt-6 p-4 border-2 border-blue-200 rounded-md;
+
+	.input-group {
+		@apply flex flex-col;
+
+		+ .input-group {
+			@apply flex flex-col;
+			@apply mt-4;
+		}
+	}
+
+	label {
+		@apply pb-1 text-gray-700;
+	}
+
+	input {
+		@apply px-2 py-1 border border-gray-300 rounded-md;
+		@apply border border-gray-300 rounded-md;
+	}
+}
+</style>
