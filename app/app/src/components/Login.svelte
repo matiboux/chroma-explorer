bef4df2052b6117eedee716afe8e0d6bf50d1592
Matiboux <matiboux@gmail.com>
2024-11-02 20:07:38 +0100

    Add basic auth login inputs

--- app/app/src/components/Login.svelte
+++ app/app/src/components/Login.svelte
@@ -1,6 +1,18 @@
 <script lang="ts">
 // Generate a random suffix for id attributes
 const idSuffix = Math.random().toString(36).substring(2)
+
+const defaultFormValues = {
+	chromadbServerUrl: 'http://localhost:8000',
+	authProvider: 'token',
+	apiToken: '',
+	basicUsername: '',
+	basicPassword: '',
+}
+
+const formValues = {
+	...defaultFormValues,
+}
 </script>
 
 <form>
@@ -11,6 +23,7 @@ const idSuffix = Math.random().toString(36).substring(2)
 		</label>
 		<input
 			type="text" name="chromadbServerUrl" required
+			bind:value={formValues.chromadbServerUrl}
 			placeholder="http://localhost:8000"
 			id={`chromadbServerUrl-${idSuffix}`}
 		/>
@@ -28,7 +41,8 @@ const idSuffix = Math.random().toString(36).substring(2)
 		</p>
 		<div class="radio-group">
 			<input
-				type="radio" name="authProvider" value="token" required
+				type="radio" name="authProvider" value="token" checked
+				bind:group={formValues.authProvider}
 				id={`authProviderToken-${idSuffix}`}
 			/>
 			<label for={`authProviderToken-${idSuffix}`}>API Token</label>
@@ -36,21 +50,56 @@ const idSuffix = Math.random().toString(36).substring(2)
 		<div class="radio-group">
 			<input
 				type="radio" name="authProvider" value="basic"
+				bind:group={formValues.authProvider}
 				id={`authProviderBasic-${idSuffix}`}
 			/>
 			<label for={`authProviderBasic-${idSuffix}`}>Basic Auth</label>
 		</div>
 	</div>
 
-	<div class="input-group">
-		<label for={`headerAuthorizationToken-${idSuffix}`}>
-			Header Authorization Token:
-		</label>
-		<input
-			type="text" name="headerAuthorizationToken" required
-			id={`headerAuthorizationToken-${idSuffix}`}
-		/>
-	</div>
+	{#if formValues.authProvider === 'token'}
+		<div class="input-group">
+			<label for={`headerAuthorizationToken-${idSuffix}`}>
+				Header Authorization Token:
+			</label>
+			<input
+				type="text" name="headerAuthorizationToken" required
+				bind:value={formValues.apiToken}
+				placeholder="API Token"
+				id={`headerAuthorizationToken-${idSuffix}`}
+			/>
+		</div>
+	{:else if formValues.authProvider === 'basic'}
+		<div class="input-groups">
+			<div class="input-group">
+				<label for={`basicUsername-${idSuffix}`}>
+					Basic Auth Username:
+				</label>
+				<input
+					type="text" name="basicUsername" required
+					bind:value={formValues.basicUsername}
+					placeholder="user"
+					id={`basicUsername-${idSuffix}`}
+				/>
+			</div>
+			<div class="input-group">
+				<label for={`basicPassword-${idSuffix}`}>
+					Basic Auth Password:
+				</label>
+				<input
+					type="password" name="basicPassword" required
+					bind:value={formValues.basicPassword}
+					placeholder="password"
+					id={`basicPassword-${idSuffix}`}
+				/>
+			</div>
+		</div>
+	{:else}
+		<p class="hint">
+			<span class="icon icon-[mdi--information-outline] icon-align"></span>
+			Please select an authentication provider.
+		</p>
+	{/if}
 
 	<div class="button-group">
 		<button type="submit">Connect</button>
@@ -63,6 +112,14 @@ const idSuffix = Math.random().toString(36).substring(2)
 form {
 	@apply flex flex-col gap-4;
 	@apply mt-4;
+
+	.input-groups {
+		@apply flex gap-4;
+
+		> .input-group {
+			@apply flex-grow;
+		}
+	}
 
 	.input-group {
 		@apply flex flex-col gap-2;
@@ -115,6 +172,7 @@ form {
 	input {
 		@apply px-2 py-1 border border-gray-300 rounded-md;
 		@apply border border-gray-300 rounded-md;
+		@apply outline-none focus:ring-2 focus:ring-offset-1 focus:ring-blue-500;
 
 		+ label {
 			@apply -mt-2 pt-1;
@@ -142,7 +200,7 @@ form {
 			enabled:hover:bg-gray-500
 			enabled:active:bg-gray-600
 			disabled:bg-gray-300
-			px-4 py-1 text-white font-semibold rounded-md
+			px-4 py-2 text-white font-semibold rounded-md
 			disabled:cursor-not-allowed
 			;
 
