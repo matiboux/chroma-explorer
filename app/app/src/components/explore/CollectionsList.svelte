7f228b1777f75422af3de5d0013acd627f0331e9
Matiboux <matiboux@gmail.com>
2024-11-03 19:53:32 +0100

    Add CollectionsList dynamic component

---
+++ app/app/src/components/explore/CollectionsList.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+const collections = [
+	'collection-1',
+	'collection-2',
+	'collection-3',
+]
+</script>
+
+<ul>
+	{#each collections as collection}
+		<li>
+			<a href="#" on:click|preventDefault={() => console.log(collection)}>
+				{collection}
+			</a>
+		</li>
+	{/each}
+</ul>
+
+<style lang="scss">
+	ul {
+		@apply w-full;
+		@apply px-1;
+		@apply list-none;
+		@apply space-y-1;
+
+		li {
+			@apply bg-white;
+			@apply rounded-lg;
+
+			a {
+				@apply text-gray-800;
+				@apply block;
+				@apply px-2 py-1;
+				@apply hover:bg-gray-100;
+				@apply rounded-lg;
+			}
+
+			&.active a, a.active {
+				@apply bg-gray-100;
+				@apply hover:bg-gray-200;
+			}
+		}
+	}
+</style>
