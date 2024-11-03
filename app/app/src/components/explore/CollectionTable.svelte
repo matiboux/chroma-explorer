ca58f91289d6ffd994751f41388cb1f918ffffbe
Matiboux <matiboux@gmail.com>
2024-11-04 00:56:32 +0100

    Add no collection selected message & hide table

--- app/app/src/components/explore/CollectionTable.svelte
+++ app/app/src/components/explore/CollectionTable.svelte
@@ -1,29 +1,40 @@
 <script lang="ts">
+import { collectionsStore } from '~/stores/collectionsStore'
 </script>
 
-<table>
-	<thead>
-		<tr>
-			<th>ID</th>
-			<th>Embedding</th>
-			<th>Document</th>
-			<th>Metadata</th>
-			<th>Actions</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<td>1</td>
-			<td><span class="icon icon-[mdi--check-circle-outline] icon-align"></span> 768</td>
-			<td>Document 1</td>
-			<td>{'{'} ... {'}'}</td>
-			<td>
-				<button type="button" class="btn">View</button>
-				<button type="button" class="btn">Edit</button>
-				<button type="button" class="btn">Delete</button>
-			</td>
-		</tr>
-</table>
+{#if !$collectionsStore.selectedCollection}
+	<p>
+		No collection selected.
+	</p>
+{:else if true}
+	<p>
+		Not implemented yet.
+	</p>
+{:else}
+	<table>
+		<thead>
+			<tr>
+				<th>ID</th>
+				<th>Embedding</th>
+				<th>Document</th>
+				<th>Metadata</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>1</td>
+				<td><span class="icon icon-[mdi--check-circle-outline] icon-align"></span> 768</td>
+				<td>Document 1</td>
+				<td>{'{'} ... {'}'}</td>
+				<td>
+					<button type="button" class="btn">View</button>
+					<button type="button" class="btn">Edit</button>
+					<button type="button" class="btn">Delete</button>
+				</td>
+			</tr>
+	</table>
+{/if}
 
 <style lang="scss">
 	table {
