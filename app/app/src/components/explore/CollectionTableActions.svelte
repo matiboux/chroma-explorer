dea3864d403f8cf5d321245f9a44ca9a851f426b
Matiboux <matiboux@gmail.com>
2024-11-04 04:08:23 +0100

    Fix texts i18n in CollectionTableActions

--- app/app/src/components/explore/CollectionTableActions.svelte
+++ app/app/src/components/explore/CollectionTableActions.svelte
@@ -5,6 +5,10 @@ import type { MultiGetResponse } from 'chromadb'
 import { configStore } from '~/stores/configStore'
 import { stateStore } from '~/stores/stateStore'
 import Button from '~/components/generic/Button.svelte'
+
+import { i18nFactory } from '~/i18n'
+export let locale: string | undefined = undefined
+const _ = i18nFactory(locale)
 </script>
 
 <div class="actions">
@@ -12,20 +16,29 @@ import Button from '~/components/generic/Button.svelte'
 	<Button buttonStyle="gray">
 		<span class="icon icon-[mdi--table-plus] icon-align"></span>
 		<span class="sr-only">
-			Refresh collections
+			{_({
+				en: 'Add new document',
+				fr: 'Ajouter un nouveau document',
+			})}
 		</span>
 	</Button>
 
 	<Button buttonStyle="gray">
 		<span class="icon icon-[mdi--table-refresh] icon-align"></span>
 		<span class="sr-only">
-			Refresh table
+			{_({
+				en: 'Refresh table',
+				fr: 'Rafraîchir le tableau',
+			})}
 		</span>
 	</Button>
 
 	<div class="collection-count">
 		<p class="badge">
-			10 documents
+			10 {_({
+				en: 'documents',
+				fr: 'documents',
+			})}
 		</p>
 	</div>
 
