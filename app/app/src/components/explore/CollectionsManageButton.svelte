21877159a635970938a43c385a3a4bddb7ad92a3
Matiboux <matiboux@gmail.com>
2024-11-26 14:06:37 +0100

    Add manage collections button

---
+++ app/app/src/components/explore/CollectionsManageButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+import { onMount } from 'svelte'
+import { ChromaClient } from 'chromadb'
+
+import { configStore } from '~/stores/configStore'
+import { stateStore, reloadCollections } from '~/stores/stateStore'
+import Button from '~/components/generic/Button.svelte'
+
+import type { Locales } from '~/i18n'
+import { i18nFactory } from '~/i18n'
+export let locale: Locales | undefined = undefined
+const _ = i18nFactory(locale)
+
+async function onClick()
+{
+	stateStore.set({
+		...stateStore.get(),
+		viewMode: 'manage',
+	})
+}
+</script>
+
+<Button on:click={onClick}>
+	<span class="icon icon-[mdi--folder-cog-outline] icon-align"></span>
+	<span class="sr-only">
+		{_({
+			en: 'Manage collections',
+			fr: 'Gérer les collections',
+		})}
+	</span>
+</Button>
+
+<style lang="scss">
+</style>
