23fc5d056f19cf99728f2d77b26a82ae7ea967e1
Matiboux <matiboux@gmail.com>
2025-01-03 01:25:16 +0100

    Update layouts & Move site config

--- app/app/src/config.ts
+++ app/app/src/config.ts
@@ -1,17 +1,5 @@
 import type { AstroConfig } from 'astro'
 
-import type Site from '~/types/Site.d.ts'
-
-export const site: Site = {
-	title: 'Chroma Explorer',
-	description: {
-		'en': 'Online tool to explore the contents of a Chroma database.',
-		'fr': 'Outil en ligne pour explorer le contenu d\'une base de données Chroma.',
-	},
-	author: 'Matiboux',
-	themeColor: '#ffffff',
-}
-
 export const i18n =
 {
 	locales: [
