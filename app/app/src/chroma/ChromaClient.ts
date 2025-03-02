a3e1c1b95d8a0372d84f6593fa44a7775f00d010
Matiboux <matiboux@gmail.com>
2025-03-02 17:22:23 +0100

    Add createCollection in chroma client

--- app/app/src/chroma/ChromaClient.ts
+++ app/app/src/chroma/ChromaClient.ts
@@ -1,4 +1,4 @@
-import { ChromaClient as BaseChromaClient, Collection } from 'chromadb'
+import { ChromaClient as BaseChromaClient, Collection, DefaultEmbeddingFunction } from 'chromadb'
 import type { ChromaClientParams } from 'chromadb'
 
 export class ChromaClient
@@ -32,7 +32,7 @@ export class ChromaClient
 		if (this.apiVersion === 'v1')
 		{
 			const response = await this.client.api.getV1Heartbeat(this.client.api.options)
-			return response['nanosecond heartbeat']!;
+			return response['nanosecond heartbeat']!
 		}
 		else
 		{
@@ -40,21 +40,68 @@ export class ChromaClient
 		}
 	}
 
-	// async createCollection(args: Parameters<BaseChromaClient['createCollection']>[0])
-	// {
-	// 	if (this.apiVersion === 'v1')
-	// 	{
-	// 		// return this.client.api.createCollectionV1(
+	async createCollection(
+		args: Parameters<BaseChromaClient['createCollection']>[0],
+	): ReturnType<BaseChromaClient['createCollection']>
+	{
+		if (this.apiVersion === 'v1')
+		{
+			// await this.client.init()
+			const response = await this.client.api.createCollectionV1(
+				this.client.tenant,
+				this.client.database,
+				{
+					name: args.name,
+					configuration: null,
+					metadata: args.metadata!,
+				},
+				this.client.api.options,
+			)
+			return new Collection(
+				(response as any).name,
+				(response as any).id,
+				this.client,
+				args.embeddingFunction ?? new DefaultEmbeddingFunction(),
+				(response as any).metadata,
+			)
+		}
+		else
+		{
+			return this.client.createCollection(args)
+		}
+	}
 
-	// 		// )
-	// 	}
-	// 	else
-	// 	{
-	// 		return this.client.createCollection(args)
-	// 	}
-	// }
-
-	// getOrCreateCollection
+	async getOrCreateCollection(
+		args: Parameters<BaseChromaClient['getOrCreateCollection']>[0],
+	): ReturnType<BaseChromaClient['getOrCreateCollection']>
+	{
+		if (this.apiVersion === 'v1')
+		{
+			// await this.client.init()
+			const response = await this.client.api.createCollectionV1(
+				this.client.tenant,
+				this.client.database,
+				{
+					name: args.name,
+					configuration: null,
+					metadata: args.metadata!,
+					get_or_create: true,
+				},
+				this.client.api.options,
+			)
+			return new Collection(
+				(response as any).name,
+				(response as any).id,
+				this.client,
+				args.embeddingFunction ?? new DefaultEmbeddingFunction(),
+				(response as any).metadata,
+			)
+		}
+		else
+		{
+			return this.client.getOrCreateCollection(args)
+		}
+	}
 
 	async listCollections(
 		args?: Parameters<BaseChromaClient['listCollections']>[0],
@@ -69,7 +116,7 @@ export class ChromaClient
 				this.client.database,
 				this.client.api.options,
 			)
-			return (collections as any).map((collection: any) => collection.name);
+			return (collections as any).map((collection: any) => collection.name)
 		}
 		else
 		{
@@ -86,7 +133,7 @@ export class ChromaClient
 	{
 		if (this.apiVersion === 'v1')
 		{
-			// await this.client.init();
+			// await this.client.init()
 			const response = await this.client.api.getCollectionV1(
 				args.name,
 				this.client.tenant,
@@ -99,7 +146,7 @@ export class ChromaClient
 				this.client,
 				args.embeddingFunction,
 				(response as any).metadata,
-			);
+			)
 		}
 		else
 		{
